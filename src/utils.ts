d4ca0ee092401c88b38db0f76ec80501337fc9f8
Jared Pereira <jared@awarm.space>
2022-02-23 21:27:49 -0500

    fix! was sorting things backwards oops

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,14 @@ import { Fact } from "../backend";
 
 export const sortByPosition =
   (key: string) =>
-  (a: Pick<Fact, "positions" | "id">, b: Pick<Fact, "positions" | "id">) => {
-    let aPosition = a.positions[key],
-      bPosition = b.positions[key];
-    if (!aPosition) {
-      if (bPosition) return 1;
-      return a.id > b.id ? -1 : 1;
-    }
-    if (!bPosition) return -1;
-    if (aPosition === bPosition) return a.id > b.id ? -1 : 1;
-    return aPosition > bPosition ? -1 : 1;
-  };
+    (a: Pick<Fact, "positions" | "id">, b: Pick<Fact, "positions" | "id">) => {
+      let aPosition = a.positions[key],
+        bPosition = b.positions[key];
+      if (!aPosition) {
+        if (bPosition) return -1;
+        return a.id > b.id ? 1 : -1;
+      }
+      if (!bPosition) return -1;
+      if (aPosition === bPosition) return a.id > b.id ? 1 : -1;
+      return aPosition > bPosition ? 1 : -1;
+    };
